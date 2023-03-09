91f86cb8f5e70bce08d738441e7063dd47bf994c
evduanG <eviatar.duany@gmail.com>
2023-03-09 12:19:02 +0200

    1.creating a limit to the number of enemies
    2.refactoring of CameraFollow 3.refactoring PlayerController > IsJump And HasPlayerCooldownExpired

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,3 +1,5 @@
+using Assets.player;
+using System;
 using System.Collections;
 using UnityEngine;
 
@@ -6,16 +8,47 @@ public class Enemy : MonoBehaviour
     public GameObject enemyPrefab;
     public float enemySpawnTime = 1;
     private Transform enemySpawnPoint;
+    private byte nunOfLiveingEnemy;
+    [SerializeField]
+    private byte maxNumOfEnemy;
+    private GameObject player;
+    public byte MunOfLiveingEnemy
+    {
+        get => nunOfLiveingEnemy;
+        set => nunOfLiveingEnemy = (byte)(value <  0 ? value : 0);
+    }
+
+    private void Awake()
+    {
+        nunOfLiveingEnemy = 0; 
+    }
+
     private void Start()
     {
+        player = GameObject.Find("Player");
+
         enemySpawnPoint = transform.Find("EnemySpawnPoint");
         StartCoroutine(EnemyGenerator());
     }
+
     IEnumerator EnemyGenerator()
     {
-        yield return new WaitForSeconds(enemySpawnTime);
-        GameObject enemy = Instantiate(enemyPrefab, enemySpawnPoint.position, Quaternion.identity);
-        enemy.GetComponent<Rigidbody2D>().velocity = new Vector2(-20f, 0f);
-        StartCoroutine(EnemyGenerator());
+        while (player != null)
+        {
+            yield return new WaitForSeconds(enemySpawnTime);
+
+            if (MunOfLiveingEnemy < maxNumOfEnemy)
+            {
+                GameObject enemy = Instantiate(enemyPrefab, enemySpawnPoint.position, Quaternion.identity);
+                enemy.GetComponent<EnemyPrefab>().onObjectDestroyed.AddListener(EnemyDestroy);
+                enemy.GetComponent<Rigidbody2D>().velocity = new Vector2(-20f, 0f);
+            }
+        }
+
+    }
+
+    public void EnemyDestroy()
+    {
+        this.MunOfLiveingEnemy--; 
     }
 }
